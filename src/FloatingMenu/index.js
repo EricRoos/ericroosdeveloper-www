844d6a4d915ea45351b02f16f039ed268b34e7bf
Eric Roos <ericroos@hey.com>
2022-01-23 16:51:28 -0600

    better aside

--- src/FloatingMenu/index.js
+++ src/FloatingMenu/index.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { Popover } from 'react-tiny-popover'
+import { useLocation } from 'react-router-dom';
 
 import {
   BsMenuUp
@@ -14,9 +15,11 @@ import Aside from '../Aside';
 
 function FloatingMenu(){
   const [ isOpen, setIsOpen ] = useState(false);
-
+  const currentLocation = useLocation();
+  
   useEffect( () => {
-  }, []);
+    setIsOpen(false);
+  }, [currentLocation]);
 
   function toggle(){
     setIsOpen(!isOpen);
@@ -26,9 +29,10 @@ function FloatingMenu(){
   return (
     <Popover
       isOpen={isOpen}
+      onClickOutside={() => setIsOpen(false)}
       content={<div className='animate__animated animate__faster animate__slideInUp md:hidden w-[80vw] m-auto px-4'><Aside className='rounded-xl rounded-br-none'/></div>}
     >
-      <button onClick={toggle} className={`md:hidden flex fixed bottom-4 right-4 rounded p-3 bg-primary z-1 justify-center items-center text-white text-3xl ${ isOpen ? 'bg-gray rounded-t-none' : '' }`}>
+      <button onClick={toggle} className={`block drop-shadow-2xl md:hidden flex fixed bottom-4 right-4 rounded p-3 bg-primary z-1 justify-center items-center text-white text-3xl ${ isOpen ? 'bg-gray rounded-t-none' : '' }`}>
         { !isOpen && <BsMenuUp /> }
         { isOpen && <IoMdClose /> }
       </button>

9066af9fa2f3dd892aabcc209de4db8d0d845dcd
Eric Roos <ericroos@hey.com>
2022-01-23 15:29:11 -0600

    example blog

--- src/Blog/example.md
+++ src/Blog/example.md
@@ -1,2 +1,6 @@
 # Test
 ## Subtitle
+
+Lorem ipsum. This is a paragraph so on and so on. *italic?* **bold**
+
+Lorem ipsum. This is a paragraph so on and so on. *italic?* **bold**
--- src/Blog/index.js
+++ src/Blog/index.js
@@ -1,11 +1,23 @@
 import ReactMarkdown from 'react-markdown';
 import ExamplePost from './example.md';
+
+import { useState, useEffect } from 'react';
 function Blog(){
-  const markdown = `foo`;
+  const [ markdown, setMarkdown ] = useState('');
+
+  const [ currentPost, setCurrentPost ] = useState(ExamplePost);
+  useEffect( () => {
+    fetch(ExamplePost).then( resp => {
+      resp.text().then( (val) => {
+        setMarkdown(val);
+      });
+    });
+  }, [currentPost])
+
   return (
-    <div className='p-8' id='blog'>
+    <div className='w-full p-8' id='blog'>
       <ReactMarkdown>
-        {ExamplePost}
+        {markdown}
       </ReactMarkdown>
     </div>
   )
